8c4481042ef13b0e67bfcfe83374fbd4495bd797
stagni <stagni@863e7ce6-8861-447e-8dcb-03987b876380>
2010-03-05 21:59:11 +0000

    Implementada interface de animation
    git-svn-id: http://xp-dev.com/svn/horus_eye/trunk@30 863e7ce6-8861-447e-8dcb-03987b876380

---
+++ src/framework/animation.cc
@@ -0,0 +1,44 @@
+#include <string.h>
+#include <stdarg.h>
+#include "animation.h"
+#include <stdlib.h>
+
+namespace framework {
+
+
+Animation::Animation(float fps, ...) {
+    int i = 0;
+    static int tmp_frame_list[256]; /*TODO*/
+    va_list arg_list;
+    
+    va_start(arg_list, fps);
+    while (1) {
+        int next_arg = va_arg(arg_list, int); 
+        if (next_arg == -1) { // Indica fim da lista de argumentos
+            break;
+        }
+        tmp_frame_list[i++] = next_arg;
+    }
+
+    n_frames_ = i;
+    frames_ = (int *) malloc(n_frames_ * sizeof(int));
+    memcpy(frames_, tmp_frame_list, n_frames_ * sizeof(int));
+
+    fps_ = fps;
+    elapsed_time_ = 0;
+    current_frame_ = 0;
+}
+
+void Animation::Update(float delta_t) {
+    elapsed_time_ += delta_t;
+    if (elapsed_time_ >= 1/fps_) {
+        current_frame_ = (current_frame_ + 1) % n_frames_;
+        elapsed_time_ = 0;
+    }
+}
+}
+
+
+
+
+        
---
+++ src/framework/animation.h
@@ -0,0 +1,36 @@
+//
+// Horus Eye - Framework
+// Copyright (C) 2010  Nucleo de Desenvolvimento de Jogos da USP
+//
+// framework/vector2D.h
+// Definicao da classe Vector2D.
+//
+#ifndef HORUSEYE_FRAMEWORK_VECTOR2D_H_
+#define HORUSEYE_FRAMEWORK_VECTOR2D_H_
+
+namespace framework {
+
+class Animation {
+  public:
+    Animation(float fps, ...);
+    ~Animation() { }
+    
+    void set_fps(float fps) { fps_ = fps; }
+    int fps() { return fps_; }
+    
+    int get_current_frame() { return current_frame_; }
+    void Update(float delta_t);
+
+  private:
+    float fps_;
+    int *frames_;
+    int n_frames_;
+    int current_frame_;
+    float elapsed_time_;
+
+};
+
+}
+#endif
+
+
