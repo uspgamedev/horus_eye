95a82dedf433edc5b96c20b324cae545f705dbdd
dkykuta <dkykuta@863e7ce6-8861-447e-8dcb-03987b876380>
2010-03-19 21:15:39 +0000

    small fixes
    git-svn-id: http://xp-dev.com/svn/horus_eye/trunk@48 863e7ce6-8861-447e-8dcb-03987b876380

---
+++ src/framework/types.h
@@ -0,0 +1,20 @@
+//
+// Horus Eye - Framework
+// Copyright (C) 2010  Nucleo de Desenvolvimento de Jogos da USP
+//
+// framework/types.h
+// Tipos genericos do framework
+//
+
+namespace framework {
+
+typedef Uint8 uint8;
+typedef Uint16 uint16;
+typedef Uint32 uint32;
+typedef Uint64 uint64;
+typedef Sint8 int8;
+typedef Sint16 int16;
+typedef Sint32 int32;
+typedef Sint64 int64;
+
+}  // namespace framework
