d99f71356b4851c7e3df4139698c9b374564f8a6
alemart <alemart@863e7ce6-8861-447e-8dcb-03987b876380>
2010-05-22 18:18:45 +0000

    - Estrutura de pastas sugerida - CMake pronto (por enquanto so' foi testado no Linux) - Novo arquivo: main.cc (lugar onde a engine e' instanciada)
    git-svn-id: http://xp-dev.com/svn/horus_eye/trunk@71 863e7ce6-8861-447e-8dcb-03987b876380

---
+++ src/game/main.cc
@@ -0,0 +1,29 @@
+//
+// Horus Eye
+// Copyright (C) 2010  Nucleo de Desenvolvimento de Jogos da USP
+//
+// game/main.cc
+// Arquivo principal do game (inclui a funcao main())
+//
+#include "../framework/engine.h"
+#include "../framework/vector2D.h"
+#include "scenes/world.h"
+using framework::Engine;
+using framework::Scene;
+using framework::Vector2D;
+using scene::World;
+
+// funcao principal do programa
+// atualmente ela simplesmente inicia
+// a engine com um World
+int main(int argc, char *argv[]) {
+    Engine *engine = Engine::reference();
+    engine->Initialize("Horus Eye", Vector2D(1024, 768), false);
+
+    Scene *world = new World;
+    engine->PushScene(world);
+    engine->Run();
+
+    engine->Release();
+    return 0;
+}
---
+++ src/game/scenes/world.h
@@ -0,0 +1,29 @@
+//
+// Horus Eye
+// Copyright (C) 2010  USPGameDev
+//
+// game/scenes/world.h
+// Definicao da classe World.
+//
+
+#ifndef HORUSEYE_GAME_SCENES_WORLD_H_
+#define HORUSEYE_GAME_SCENES_WORLD_H_
+
+#include "../../framework/scene.h"
+
+namespace scene {
+
+// Classe World
+// O World e' uma cena onde o jogo se desencadeara'. O World contem
+// elementos como: heroi, mumias, cenario e hud.
+// AVISO (podem excluir): criei o esqueleto da classe para poder
+// testar a compilacao do jogo. :P
+class World : public framework::Scene {
+  public:
+    World() : Scene() { }
+    virtual ~World() { }
+};  // class World
+
+}  // namespace scene
+
+#endif  // HORUSEYE_GAME_SCENES_WORLD_H_
