e15940e28f5f316725c75e41c46bf53713297056
samuelplaca <samuelplaca@863e7ce6-8861-447e-8dcb-03987b876380>
2010-05-27 21:28:28 +0000

    Grupo 1: Tinhamos esquecido de adicionar o world.cc
    git-svn-id: http://xp-dev.com/svn/horus_eye/trunk@81 863e7ce6-8861-447e-8dcb-03987b876380

---
+++ src/game/scenes/world.cc
@@ -0,0 +1,67 @@
+//
+// Horus Eye
+// Copyright (C) 2010  USPGameDev
+//
+// game/scenes/world.cc
+// Implementacao da classe World.
+//
+
+#include <list.h>
+#include "../../framework/scene.h"
+#include "../../framework/vector2D.h"
+#include "../sprites/worldobject.h"
+#include "../sprites/hero.h"
+#include "world.h"
+
+namespace scene {
+
+// Construtor
+World::World() : world_layer_() {
+    AddLayer(world_layer_);
+}
+
+// Destrutor
+World::~World() {
+}
+
+void World::Update(float delta_t) {
+    std::list<sprite::WorldObject*>::iterator i, j;
+    Scene::Update(delta_t);
+    for (i = collidable_.begin(); i != collidable_.end(); ++i)
+        for (j = i, ++j; j != collidable_.end(); ++j)
+            if ((*i).IsColliding(*j)) {
+                (*i).CollidesWith(*j);
+                (*j).CollidesWith(*i);
+            }
+}
+
+// Nao nos importamos com a ordem dos WorldObjects nas listas;
+// Decidimos fazer as adicoes e remocoes no comeco das listas por padrao.
+void World::AddWorldObject(sprite::WorldObject* new_object) {
+    world_objects_.push_front(new_object);
+    collidable_.push_front(new_object);
+    world_layer_.AddSprite(new_object);
+}
+
+void World::AddFloor(sprite::Floor* new_floor) {
+    world_objects_.push_front(new_floor);
+    world_layer_.AddSprite(new_floor);
+}
+
+void World::FinishLevel(bool goodEnd) {
+}
+
+int World::CountRemainingEnemies() {
+    return 0; // Aguardando implementacao da classe Mumia
+}
+
+
+// get Hero
+Hero * World::hero() {
+    return hero_;
+}
+
+void World::RemoveInactiveObjects() {
+}
+
+} // namespace scene
