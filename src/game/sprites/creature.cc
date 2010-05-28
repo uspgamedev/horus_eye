1153df1563033e17587065c290e56147dd6bf056
DarkSzero <DarkSzero@863e7ce6-8861-447e-8dcb-03987b876380>
2010-05-28 20:18:03 +0000

    Grupo 3: Corrigindo padrões, clarificando o codigo e implementando parte da colisao que eu esqueci.
    git-svn-id: http://xp-dev.com/svn/horus_eye/trunk@87 863e7ce6-8861-447e-8dcb-03987b876380

---
+++ src/game/sprites/creature.cc
@@ -0,0 +1,29 @@
+//
+// Horus Eye
+// Copyright (C) 2010  USPGameDev
+//
+// game/sprites/creature.cc
+// Implementacao da classe Creature.
+//
+
+#include "creature.h"
+
+using namespace framework;
+
+namespace sprite {
+
+void Creature::Move(Vector2D direction, float delta_t) {
+    // TODO: implementar essa funcao
+}
+
+void Creature::CollidesWith(Wall * obj);
+    Vector2D distance = this->world_position() - obj->world_position();
+    float curDist = sqrt(Vector2D::InnerProduct(distance, distance));
+    float angle = atan2(distance.y, distance.x);
+    this->set_world_position(
+        obj->world_position() +
+        new Vector2D(cos(angle), sin(angle)) *
+        ((this->collision_radius() + obj->collision_radius()) / curDist )  );
+}
+
+}  // namespace sprite
