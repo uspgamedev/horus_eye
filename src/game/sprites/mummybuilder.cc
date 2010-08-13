32c8e6bdb124fd0b64ea2982942276036d865b69
natan <natan@863e7ce6-8861-447e-8dcb-03987b876380>
2010-08-13 20:03:27 +0000

    mumia gigante com hotspot
    git-svn-id: http://xp-dev.com/svn/horus_eye/trunk@256 863e7ce6-8861-447e-8dcb-03987b876380

--- src/game/sprites/mummybuilder.cc
+++ src/game/sprites/mummybuilder.cc
@@ -10,10 +10,13 @@ Mummy * MummyBuilder::standard_mummy() {
 
 Mummy * MummyBuilder::big_mummy() {
     ImageFactory image_factory;
-    Mummy *mummy = new Mummy(image_factory.BigMummyImage());
+	Image* big_mummy_image = image_factory.BigMummyImage();
+    Mummy *mummy = new Mummy(big_mummy_image);
     mummy->set_life(Constants::BIG_MUMMY_LIFE);
     mummy->set_speed(Constants::BIG_MUMMY_SPEED);
     mummy->set_bound(Constants::BIG_MUMMY_RADIUS);
+	mummy->set_hotspot(Vector2D(big_mummy_image->frame_size().x / 2.0, 
+								big_mummy_image->frame_size().y*6.0 / 7.0));
 
     return mummy;
 }
