d94784e2c50d05304d5d1bd632752d6a747cb752
Kazuo <Kazuo@863e7ce6-8861-447e-8dcb-03987b876380>
2010-09-08 18:31:53 +0000

    Wil & Goroba: Poções balançando aleatoriamente (dessa vez propositalmente).
    git-svn-id: http://xp-dev.com/svn/horus_eye/trunk@369 863e7ce6-8861-447e-8dcb-03987b876380

--- src/game/sprites/item.cc
+++ src/game/sprites/item.cc
@@ -15,13 +15,17 @@
 #define CENTER_X    Constants::POTION_SPRITE_CENTER_X
 #define CENTER_Y    Constants::POTION_SPRITE_CENTER_Y
 #define HEIGHT      Constants::POTION_HEIGHT
+#define PI 3.141592654f
+#define PERIOD (2.0f*PI/3.0f)
 
 namespace sprite {
 
 using namespace framework;
 using namespace utils;
 
-Item::Item(framework::Image* img) {
+
+
+Item::Item(framework::Image* img) : total_time_((1.0f*rand()/RAND_MAX)*PERIOD) {
     Initialize(img);
     set_hotspot(Vector2D(CENTER_X, CENTER_Y + HEIGHT));
     this->bound_ = new CircleObject(0.15f);
@@ -32,6 +36,7 @@ Item::Item(framework::Image* img) {
 void Item::Update(float delta_t) {
 	WorldObject::Update(delta_t);
     total_time_ += delta_t;
+    if (total_time_ >= PERIOD) total_time_ -= PERIOD;
     set_hotspot(Vector2D(CENTER_X, CENTER_Y + HEIGHT + 10.0f*cos(3.0f*total_time_)));
 }
 
