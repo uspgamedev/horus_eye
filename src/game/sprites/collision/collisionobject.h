9741d28351890ecd10e571876c6b5af1b2d53ca3
henriquegemignani <henrique@gemignani.org>
2011-10-20 11:12:14 -0200

    New macros for collision declaration!

--- src/game/sprites/collision/collisionobject.h
+++ src/game/sprites/collision/collisionobject.h
@@ -3,6 +3,20 @@
 
 namespace sprite {
 
+#define COLLISION_BEGIN struct Collisions {
+#define COLLISION_END	};
+#define COLLISION_ADD(OWNER, NAME) class NAME : public CollisionObject { \
+	protected:	OWNER *owner_; \
+	public:		NAME ( OWNER *onwer) : owner_(onwer) {} \
+	void Handle(WorldObject* obj); };
+
+#define COLLISION_ADD_INLINE(OWNER, NAME, CODE) class NAME : public CollisionObject { \
+	protected:	OWNER *owner_; \
+	public:		NAME ( OWNER *onwer) : owner_(onwer) {} \
+	void Handle(WorldObject* obj) { CODE } };
+
+#define COLLISION_IMPLEMENT(ONWER, NAME, DATA) void ONWER::Collisions::NAME::Handle(WorldObject *DATA)
+
 class WorldObject;
 class CollisionObject {
   public:
