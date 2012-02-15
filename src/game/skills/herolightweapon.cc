8c54278fb32b0a3a71a7fbb00c0be36dd798c141
Jefferson Serafim <jefferson.a@gmail.com>
2012-02-15 21:57:03 -0200

    Fixing includes to reflect change in node.h (from ugdk)

--- src/game/skills/herolightweapon.cc
+++ src/game/skills/herolightweapon.cc
@@ -1,6 +1,7 @@
 #include <ugdk/math/vector2D.h>
 #include <ugdk/audio/audiomanager.h>
 #include <ugdk/action/animation.h>
+#include <ugdk/graphic.h>
 #include <ugdk/graphic/node.h>
 #include <ugdk/graphic/drawable/sprite.h>
 #include <ugdk/graphic/drawable/texturedrectangle.h>
