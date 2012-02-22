ddc7b44778a8a1975051779a11a3785c772f2ef7
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2012-02-22 00:25:47 -0200

    Added a pre-build script for VS that guarantees that config.h exists. Requires powershell script execution permission set. Run "set-executionpolicy unrestricted" from an administrator x86 powershell window.

---
+++ configgenerator.ps1
@@ -0,0 +1,6 @@
+if((Test-Path src\game\config.h) -And (((Get-Item src\game\config.h.in).LastWriteTime - (Get-Item src\game\config.h).LastWriteTime).TotalMinutes -gt 0)) {
+	Remove-Item src\game\config.h
+}
+if(((Test-Path src\game\config.h) -eq $False) -And (Test-Path src\game\config.h.in)) {
+	Get-Content src\game\config.h.in | ForEach-Object { $_ -replace "@GAME_VERSION@", "1.1.1 Alpha" -replace "@GAME_INSTALL_DIR@", "./data" } | Set-Content src\game\config.h
+}
