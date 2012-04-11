f2072bf9e1b8e2375d3955334387b4e44c73af9a
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2012-04-11 17:56:20 -0300

    Missing files.

---
+++ src/game/sprites.h
@@ -0,0 +1,15 @@
+#ifndef HORUSEYE_GAME_SPRITES_H_
+#define HORUSEYE_GAME_SPRITES_H_
+
+namespace sprite {
+
+class Condition;
+class Explosion;
+class Follower;
+class Item;
+class ItemEvent;
+class WorldObject;
+
+}  // namespace component
+
+#endif
