3548621882bd2a3369a5e7e20742d1ecc8388183
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2012-05-12 00:21:38 -0300

    Removed projectile.cc

--- src/game/sprites/projectiles/projectile.h
+++ src/game/sprites/projectiles/projectile.h
@@ -9,9 +9,21 @@ namespace sprite {
 class Projectile : public WorldObject {
   private: typedef WorldObject super;
   public:
-    Projectile(double speed, int duration, ugdk::Vector2D &dir);
-    void Move(double delta_t);
-    virtual void Update(double delta_t);
+    Projectile(double speed, int duration, const ugdk::Vector2D &dir)
+    :   WorldObject(duration / 1000.0),
+        speed_(speed),
+        direction_(dir.Normalize()) {}
+
+    void Move(double delta_t) {
+        ugdk::Vector2D velocity = direction_ * (speed_ * delta_t);
+        set_world_position(this->world_position() + velocity);
+    }
+
+    virtual void Update(double delta_t) {
+        super::Update(delta_t);
+        if (is_active())
+            this->Move(delta_t);
+    }
 
   protected:
     double speed_;
