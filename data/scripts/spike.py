aab6cc07708e9b945bdee7319cbd5aa2bfe6c14a
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2012-07-02 20:16:04 -0300

    Spike is always the same drawable again.

--- data/scripts/spike.py
+++ data/scripts/spike.py
@@ -2,14 +2,9 @@ from ugdk.ugdk_drawable import Sprite
 from ugdk.ugdk_base import ResourceManager_GetSpritesheetFromTag as GetSpritesheetFromTag, ResourceManager_GetAnimationSetFromFile as GetAnimationSetFromFile
 from ugdk.pyramidworks_geometry import *
 
-import random
-
-names = ["images/explosion.png", "images/pharaoh_120x140.png", "images/shield.png" ]
-
 def generate():
     d = {}
-    random.shuffle(names)
-    d['drawable'] = Sprite(GetSpritesheetFromTag(names[0]), GetAnimationSetFromFile("animations/explosion.gdd"))
+    d['drawable'] = Sprite(GetSpritesheetFromTag("images/explosion.png"), GetAnimationSetFromFile("animations/explosion.gdd"))
     d['drawable'].SelectAnimation("HERO_FIREBALL_WEAPON")
     
     d['collision'] = {
