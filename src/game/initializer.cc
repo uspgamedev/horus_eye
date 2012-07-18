efc81026ab72690cec17364f43a6b6de16b1ff9a
Gustavo Teixeira da Cunha Coelho <gustavotdaccoelho@gmail.com>
2012-07-18 19:14:10 -0300

    Migrating more spritesheets.

--- src/game/initializer.cc
+++ src/game/initializer.cc
@@ -22,10 +22,10 @@ static void CreateFixedSpritesheet(const char* path, int frame_width, int frame_
 void LoadSpritesheets() {
     //CreateFixedSpritesheet(   "images/eye.png"                , 128,  96, ugdk::Vector2D());
     //CreateFixedSpritesheet(   "images/sprite-sheet_MOD3.png"  , 110, 110, ugdk::Vector2D( 55.0, 102.0)); // Kha
-    CreateFixedSpritesheet("images/mummy_blue_120x140.png" , 120, 140, ugdk::Vector2D( 60.0, 120.0)); // Regular Mummy
-    CreateFixedSpritesheet("images/mummy_green_120x140.png", 120, 140, ugdk::Vector2D( 60.0, 120.0)); // Giant Mummy
-    CreateFixedSpritesheet("images/pharaoh_120x140.png"    , 120, 140, ugdk::Vector2D( 60.0, 120.0)); // Pharaoh
-    CreateFixedSpritesheet("images/mummy_red_120x140.png"  , 120, 140, ugdk::Vector2D( 60.0, 120.0)); // Shooting Mummy
+    //CreateFixedSpritesheet("images/mummy_blue_120x140.png" , 120, 140, ugdk::Vector2D( 60.0, 120.0)); // Regular Mummy
+    //CreateFixedSpritesheet("images/mummy_green_120x140.png", 120, 140, ugdk::Vector2D( 60.0, 120.0)); // Giant Mummy
+    //CreateFixedSpritesheet("images/pharaoh_120x140.png"    , 120, 140, ugdk::Vector2D( 60.0, 120.0)); // Pharaoh
+    //CreateFixedSpritesheet("images/mummy_red_120x140.png"  , 120, 140, ugdk::Vector2D( 60.0, 120.0)); // Shooting Mummy
     
     CreateFixedSpritesheet("images/blue_fire_ball.png"     ,  32,  32, ugdk::Vector2D( 16.0,  16.0)); // Magic Missile
     CreateFixedSpritesheet("images/yellow_fire_ball.png"   ,  32,  32, ugdk::Vector2D( 16.0,  16.0)); // Blue Gem
