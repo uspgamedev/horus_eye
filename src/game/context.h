36a572cea23f19ff492399555e0411f1ff094daa
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2012-08-10 17:06:08 -0300

    Added function context::MakeCollisionObject

--- src/game/context.h
+++ src/game/context.h
@@ -4,6 +4,8 @@
 
 #include <string>
 
+#include <pyramidworks/collision.h>
+
 #include "game/sprites/worldobject.h"
 
 namespace context {
@@ -11,6 +13,8 @@ namespace context {
 sprite::WorldObject* WorldObjectByTag (const std::string& tag);
 
 void CreateTag (sprite::WorldObject* obj, const std::string& tag);
+
+pyramidworks::collision::CollisionObject* MakeCollisionObject (sprite::WorldObject* obj);
 
 } // namespace context
 
