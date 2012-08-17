82bb1176d4191372f20cc043085aace464dcc6ed
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2012-08-17 19:05:53 -0300

    Using downloadable switch graphics.

--- data/scripts/spritesheets/switch.py
+++ data/scripts/spritesheets/switch.py
@@ -1,7 +1,7 @@
 import ugdk.ugdk_math
 
 def get_name(s):
-    return "images/switch/%04d.png" % s
+    return "resources/images/switch/%04d.png" % s
 
 file = [get_name(i+1) for i in range(120)]
 frames = [[0, 0, 256, 256, ugdk.ugdk_math.Vector2D(128, 230), i ] for i in range(120)]
