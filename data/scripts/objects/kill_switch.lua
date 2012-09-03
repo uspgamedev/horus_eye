7f9760f9b66af1ff44f1a22cd88cf99e37cbc495
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2012-09-03 14:38:42 -0300

    Added kill_switch script object

---
+++ data/scripts/objects/kill_switch.lua
@@ -0,0 +1,57 @@
+
+require "ugdk.drawable"
+require "ugdk.action"
+require "pyramidworks.geometry"
+require "component"
+require "context"
+
+local Sprite = ugdk_drawable.Sprite
+local Rect = pyramidworks_geometry.Rect
+
+local function make_switch ()
+  local switch = proxy "Observer"
+  switch.activated = false
+  switch.sprite = Sprite("switch", "animations/switch.gdd")
+  switch.sprite:SelectAnimation "SWITCH_OFF"
+  switch.sprite:AddObserverToAnimation(switch)
+  function switch:Tick ()
+    if self.activated then
+      self.sprite:SelectAnimation "SWITCH_ON"
+    end
+  end
+  return switch
+end
+
+function generate ()
+  local descriptor = {}
+  local switch = make_switch()
+  
+  descriptor.drawable = switch.sprite
+
+  descriptor.collision = {
+    class = "Switch",
+    shape = Rect(0.8, 0.8),
+    known_collision = {},
+    custom_collision = {
+      Projectile = function(self, obj)
+        if not switch.activated then
+          for i=1,3 do
+            local door = context.WorldObjectByTag("DOOR-1-"..i)
+            if not door then
+              print "door not found"
+            else
+              door:Die()
+              switch.activated = true
+              switch.sprite:SelectAnimation "SWITCH_START"
+            end
+          end
+        end
+      end
+    }
+  }
+
+  return descriptor
+end
+
+function build (wobj)
+end
