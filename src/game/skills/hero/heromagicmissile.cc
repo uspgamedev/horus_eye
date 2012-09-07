fe13b575c08f7444427f9c3ac54040c86dea5db9
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2012-09-07 20:38:32 -0300

    Magic missile and fireball.

---
+++ src/game/skills/hero/heromagicmissile.cc
@@ -0,0 +1,35 @@
+#include "game/skills/heroskills.h"
+
+#include <ugdk/math/vector2D.h>
+#include <ugdk/audio/audiomanager.h>
+#include <ugdk/base/engine.h>
+
+#include "game/scenes/world.h"
+#include "game/builders/projectilebuilder.h"
+#include "game/components/caster.h"
+#include "game/utils/settings.h"
+#include "game/utils/constants.h"
+
+namespace skills {
+
+using namespace scene;
+using namespace ugdk;
+using utils::Constants;
+
+static void HeroMagicMissileUse(component::Caster* caster) {
+    World *world = WORLD();
+
+    Vector2D versor = (caster->aim().destination_ - caster->aim().origin_).Normalize(),
+             pos = caster->aim().origin_;
+    builder::ProjectileBuilder proj;
+    world->AddWorldObject(proj.MagicMissile(versor), pos);
+
+    if(utils::Settings::reference()->sound_effects())
+        Engine::reference()->audio_manager()->LoadSample("samples/fire.wav")->Play();
+}
+
+CombatArt* HeroMagicMissile() {
+    return new CombatArt(NULL, HeroMagicMissileUse, 0);
+}
+
+}
---
+++ src/game/skills/heroskills.h
@@ -0,0 +1,13 @@
+#ifndef HORUSEYE_GAME_SKILLS_HEROSKILLS_H_
+#define HORUSEYE_GAME_SKILLS_HEROSKILLS_H_
+
+#include "game/skills/combatart.h"
+
+namespace skills {
+
+CombatArt* HeroMagicMissile();
+CombatArt* HeroFireball();
+
+} // skills
+
+#endif /* HORUSEYE_GAME_SKILLS_HEROSKILLS_H_ */
