14342c426cd5e7e6f6167b888b7ae8e102e03ee5
Rewasvat <fernando_aluani@hotmail.com>
2012-10-05 18:18:31 -0300

    implemented CheckTargetVisible block

---
+++ src/game/ai/blocks/checktargetvisible.cc
@@ -0,0 +1,37 @@
+#include "game/ai/blocks/checktargetvisible.h"
+#include "game/ai/aidata.h"
+#include "game/sprites/worldobject.h"
+#include "game/context.h"
+#include <string>
+
+namespace ai {
+namespace blocks {
+
+void CheckTargetVisible::Start() {
+}
+
+AIModule::Status CheckTargetVisible::Update(double dt, AIData* data) {
+	sprite::WorldObject* owner = parent_->root()->owner();
+	
+	if (owner->animation()->is_uninterrutible() ) return AIModule::DORMANT;
+    if (owner->is_active() ) return AIModule::DORMANT;
+
+    if (target_ == NULL && target_tag_.size() > 0) {
+        target_ = context::WorldObjectByTag(target_tag_);
+    }
+
+	if (target_ != NULL && vision_strategy_.IsVisible(owner, target_->world_position())) {
+		/*mummy->set_standing(false);
+		mummy->set_saw_hero(true);
+		mummy->set_last_known_hero_pos( WORLD()->hero()->world_position() );*/
+		data->set_direction(target_->world_position() );
+		return AIModule::ACTIVE;
+	}
+	return AIModule::DORMANT;
+}
+
+void CheckTargetVisible::Finish() {
+}
+
+}
+}
---
+++ src/game/ai/blocks/checktargetvisible.h
@@ -0,0 +1,37 @@
+#ifndef HORUSEYE_GAME_AI_BLOCK_CHECKTARGETVISIBLE_H_
+#define HORUSEYE_GAME_AI_BLOCK_CHECKTARGETVISIBLE_H_
+
+#include "game/sprites.h"
+#include "game/ai.h"
+#include "game/ai/logicblock.h"
+#include "game/utils/visionstrategy.h"
+#include <string>
+
+namespace ai {
+namespace blocks {
+
+class CheckTargetVisible : public LogicBlock {
+
+public:
+	CheckTargetVisible() : target_(0), target_tag_("") {}
+	~CheckTargetVisible() {}
+
+	virtual void Start();
+	virtual AIModule::Status Update(double dt, AIData* data);
+	virtual void Finish();
+
+    sprite::WorldObject* target() const { return target_; }
+    void set_target(sprite::WorldObject* target) { target_ = target; }
+    std::string& target_tag() const { return target_tag_; }
+    void set_target_tag(std::string& tag) { target_tag_ = tag; }
+
+protected:
+	utils::VisionStrategy vision_strategy_;
+    
+    sprite::WorldObject* target_;
+    std::string target_tag_;
+};
+
+}
+}
+#endif // CHECKTARGETVISIBLE_H_
