ad7382bd3ede1e090a58cb08cc409c6fb7405a8b
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2012-11-23 17:48:35 -0200

    Derp

--- src/game/components/condition.cc
+++ src/game/components/condition.cc
@@ -20,7 +20,7 @@ static bool deleteeffect(const shared_ptr<Effect>& effect) {
 }
 
 void Condition::Update(double dt) {
-    for (list<shared_ptr<Effect>>::iterator i = effects_.begin(); i != effects_.end(); ++i)
+    for (list< shared_ptr<Effect> >::iterator i = effects_.begin(); i != effects_.end(); ++i)
         (*i)->Update(dt);
     effects_.remove_if(deleteeffect);
 }
@@ -33,7 +33,7 @@ bool Condition::AddEffect(const std::tr1::shared_ptr<sprite::Effect>& new_effect
 
 int Condition::CountEffectsByName(const std::string& name) const {
     int result = 0;
-    for (list<shared_ptr<Effect>>::const_iterator i = effects_.begin(); i != effects_.end(); ++i)
+    for (list< shared_ptr<Effect> >::const_iterator i = effects_.begin(); i != effects_.end(); ++i)
         result += static_cast<int>((*i)->name() == name);
     return result;
 }
