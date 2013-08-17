fefcff4dac03d0ea9d7b0cbfdea197011d869fe1
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2013-08-17 01:56:04 -0300

    Direction::FromWorldVector returns empty Direction for zero vector.

--- src/game/components/direction.cc
+++ src/game/components/direction.cc
@@ -17,28 +17,32 @@ Direction Direction::FromScreenVector(const ugdk::math::Vector2D& versor) {
 }
 
 Direction Direction::FromWorldVector(const ugdk::math::Vector2D& versor) {
-	static double PI_8 = std::atan(1.0) / 2.0;
-	double angle = versor.Angle();
+    static double PI_8 = std::atan(1.0) / 2.0;
 
-	if(       angle < -7*PI_8) {
-		return Direction(DOWN | LEFT);
-	} else if(angle < -5*PI_8) {
-		return Direction(DOWN);
-	} else if(angle < -3*PI_8) {
-		return Direction(DOWN | RIGHT);
-	} else if(angle < -1*PI_8) {
-		return Direction(RIGHT);
-	} else if(angle <  1*PI_8) {
-		return Direction(UP | RIGHT);
-	} else if(angle <  3*PI_8) {
-		return Direction(UP);
-	} else if(angle <  5*PI_8) {
-		return Direction(UP | LEFT);
-	} else if(angle <  7*PI_8) {
-		return Direction(LEFT);
-	} else {
-		return Direction(DOWN | LEFT);
-	}
+    if(versor.LengthSquared() == 0)
+        return Direction();
+
+    double angle = versor.Angle();
+
+    if(       angle < -7*PI_8) {
+        return Direction(DOWN | LEFT);
+    } else if(angle < -5*PI_8) {
+        return Direction(DOWN);
+    } else if(angle < -3*PI_8) {
+        return Direction(DOWN | RIGHT);
+    } else if(angle < -1*PI_8) {
+        return Direction(RIGHT);
+    } else if(angle <  1*PI_8) {
+        return Direction(UP | RIGHT);
+    } else if(angle <  3*PI_8) {
+        return Direction(UP);
+    } else if(angle <  5*PI_8) {
+        return Direction(UP | LEFT);
+    } else if(angle <  7*PI_8) {
+        return Direction(LEFT);
+    } else {
+        return Direction(DOWN | LEFT);
+    }
 }
 
 }  // namespace component
