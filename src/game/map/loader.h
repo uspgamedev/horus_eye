3b0fb83ba56de322a5359388d2a4e05ccef98052
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2013-08-17 03:12:38 -0300

    Using clang for travis.

--- src/game/map/loader.h
+++ src/game/map/loader.h
@@ -1,5 +1,5 @@
 #ifndef HORUSEYE_GAME_MAP_LOADER_H_
-#define HORUSEYE_GAME_MAP_LOADER_H
+#define HORUSEYE_GAME_MAP_LOADER_H_
 
 #include <string>
 #include <ugdk/math/integer2D.h>
@@ -13,4 +13,4 @@ Room* LoadRoom(const std::string& name, const ugdk::script::VirtualObj& room_scr
 
 }
 
-#endif // HORUSEYE_GAME_MAP_LOADER_H
+#endif // HORUSEYE_GAME_MAP_LOADER_H_
