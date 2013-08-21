403f7020011890ea26a76817d7f547641825e477
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2013-08-21 17:47:10 -0300

    Isolated spike room script.

--- data/scripts/campaigns/demo/levels/demo_lvl_2.lua
+++ data/scripts/campaigns/demo/levels/demo_lvl_2.lua
@@ -24,65 +24,6 @@ start_position = {"entrance", 2, roomsize / 2}
 width = 100
 height = 100
 
-
-
-event.Clear "FALL_SPIKES"
-spike_room = {
-  neighborhood = { "divergence" },
-  width = 9,
-  height = 6,
-  matrix = [[
-%%%%%%%%%
-%.......%
-%........
-%........
-%.......#
-#########
-]],
-  recipes = {
-    ["fall-spikes-trigger"] = {
-      property = "trigger",
-      params = { activates = "FALL_SPIKES", delay = 0.4 }
-    },
-    ["spike-trap"] = { property = "spike-trap" },
-    ["spike-trap-event"] = {
-      property = "event_region",
-      params = {
-        shape = pyramidworks_geometry.Rect(1.0, 4.0),
-        callback = function (region)
-          local room = region:current_room()
-          event.Register(
-            "FALL_SPIKES",
-            function ()
-              room:MakeRecipe("spike-trap", vec2(5, 1))
-              room:MakeRecipe("spike-trap", vec2(5, 2))
-              room:MakeRecipe("spike-trap", vec2(5, 3))
-              room:MakeRecipe("spike-trap", vec2(5, 4))
-              event.Clear "FALL_SPIKES"
-            end
-          )
-          room:MakeRecipe "fall-spikes-trigger"
-        end
-      }
-    },
-    ["urn"] = { property = "urn" },
-    ["door-left"] = { property = "closed-door", params = { dir = "Left" } },
-    ["door-right"] = { property = "closed-door", params = { dir = "Right" } },
-    ["eventbutton"] = {
-      property = "event_button",
-      params = { "DIVERGENCE-DOOR-COUNTER" }
-    }
-  },
-  collision_classes = {
-    {"Spike"},
-    {"EventArea"}
-  },
-  setup = function(self)
-    self:MakeRecipe("eventbutton", vec2(2,2.5))
-    self:MakeRecipe("spike-trap-event", vec2(5,2.5))
-  end
-}
-
 fireball_room = {
   neighborhood = { "divergence" },
   width = 9,
