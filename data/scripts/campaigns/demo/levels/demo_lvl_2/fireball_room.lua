699b2713cec27681183dac7b04f5aeb68fe2aab5
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2013-08-21 17:55:38 -0300

    Isolated fireball room script.

---
+++ data/scripts/campaigns/demo/levels/demo_lvl_2/fireball_room.lua
@@ -0,0 +1,49 @@
+
+require "ugdk.math"
+require "event"
+
+local vec2 = ugdk_math.Vector2D
+
+neighborhood = { "divergence" }
+
+width = 9
+height = 6
+
+matrix = [[
+%%%%%%%%%
+%.......%
+........%
+........%
+#.......%
+####&####
+]]
+
+recipes = {
+  ["fireball-trap"] = {
+    property = "fireball-trap",
+    params = {dir = vec2(0, -1)}
+  },
+  ["fireball-event"] = {
+    property = "event_region",
+    params = {
+      shape = pyramidworks_geometry.Rect(1.0, 4.0),
+      callback = function (region)
+        local room = region:current_room()
+        room:MakeRecipe("fireball-trap", vec2(4, 5))
+      end
+    }
+  },
+  ["eventbutton"] = {
+    property = "event_button",
+    params = { "DIVERGENCE-DOOR-COUNTER" }
+  }
+}
+
+collision_classes = {
+  { "EventArea" }
+}
+
+function setup (room)
+  room:MakeRecipe("eventbutton", vec2(4,2.5))
+  room:MakeRecipe("fireball-event", vec2(4,2.5))
+end
